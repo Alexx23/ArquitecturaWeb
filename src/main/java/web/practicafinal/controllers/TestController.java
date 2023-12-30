c2015a09c4f7931974213063f878187a9bf5420e
Alexx23 <alexandromadrid10@gmail.com>
2023-12-30 13:56:51 +0100

    Improved TestController

--- src/main/java/web/practicafinal/controllers/TestController.java
+++ src/main/java/web/practicafinal/controllers/TestController.java
@@ -6,12 +6,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import web.practicafinal.models.Actor;
 import web.practicafinal.models.controllers.ModelController;
 import web.practicafinal.utils.CustomLogger;
+import web.practicafinal.utils.Response;
 
 /**
  *
@@ -27,26 +25,14 @@ public class TestController extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
         Actor actor = new Actor();
         actor.setName("test");
         try {
             ModelController.getActor().create(actor);
-            CustomLogger.info("Actor creado");
+            Response.outputMessage(response, 200, "Actor creado.");
         } catch (Exception ex) {
-            Logger.getLogger(TestController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Test</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet en " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            CustomLogger.errorThrow(TestController.class.getName(), ex);
+            Response.outputMessage(response, 500, "Ha ocurrido un error interno.");
         }
     }
 }
